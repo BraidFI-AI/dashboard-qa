5f7399c995965eae48142a46f781c4d91ed370a6
Usama Hafeez <hafeezkardar000@gmail.com>
2024-09-08 21:58:31 +0500

    uncommented required code whcih was commented for testing

--- src/core/components/Drawer/header_buttons/resolve_alert.tsx
+++ src/core/components/Drawer/header_buttons/resolve_alert.tsx
@@ -142,7 +142,7 @@ const ResolveAlertButton = () => {
           setOfacHit(null);
         }
       } else {
-        // setIsOpen(false);
+        setIsOpen(false);
       }
     }
   }, [alert]);
