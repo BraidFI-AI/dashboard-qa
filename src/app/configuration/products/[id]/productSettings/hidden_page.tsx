2cdf17290b5dc48bbb35aced8ccb3165265d09f9
Usama Hafeez <hafeezkardar000@gmail.com>
2024-10-05 22:30:14 +0500

    ach hold chagne

--- src/app/configuration/products/[id]/productSettings/hidden_page.tsx
+++ src/app/configuration/products/[id]/productSettings/hidden_page.tsx
@@ -32,6 +32,7 @@ import MyExpandableButton from "@/core/components/Button/MyExpandableButton";
 import Colorful from "@uiw/react-color-colorful";
 import MyTextField from "@/core/components/TextField/MyTextField";
 import OnbaordingSettings from "./components/OnboardingSettings";
+import React from "react";
 
 const ProductSettings = ({ params }: { params: { id: string } }) => {
   const dispatch = useAppDispatch();
@@ -241,7 +242,7 @@ const ProductSettings = ({ params }: { params: { id: string } }) => {
                   <Box className="flex flex-col">
                     <MyText>ACH Hold Days</MyText>
                     <MyControlledTextField
-                      name="achHold"
+                      name="achHoldDays"
                       displayName="ACH Hold Days"
                       control={fundsControl}
                       errors={fundsErrors}
@@ -252,7 +253,7 @@ const ProductSettings = ({ params }: { params: { id: string } }) => {
                               required: true,
                             }
                       }
-                      value={fundsAvailability.achHold.toString()}
+                      value={fundsAvailability.achHoldDays.toString()}
                     />
                     <Box className="pb-4"></Box>
                     <MyText>Inter Issuer Hold</MyText>
