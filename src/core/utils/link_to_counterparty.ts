68af4ac12f822b38cf48a5158ea73dc47d4f4ed4
Usama Hafeez <hafeezkardar000@gmail.com>
2025-06-06 00:28:28 +0500

    BRAID-3180

--- src/core/utils/link_to_counterparty.ts
+++ src/core/utils/link_to_counterparty.ts
@@ -6,12 +6,17 @@ export default function linkToCounterparty(counterparty: Counterparty | null) {
   }
   if (counterparty.productId != null) {
     return `/configuration/products/${counterparty.productId}/counterparties/${counterparty.id}`;
-  } else if (counterparty.accountId != null) {
-    return `/accounts/${counterparty.accountId}/counterparties/${counterparty.id}`;
+  } else if (
+    counterparty.accountNumber != null ||
+    counterparty.accountId != null
+  ) {
+    return `/accounts/${
+      counterparty.accountNumber ?? counterparty.accountId
+    }/counterparties/${counterparty.id}`;
   } else if (counterparty.businessId != null) {
     return `/businesses/${counterparty.businessId}/counterparties/${counterparty.id}`;
-  } else if (counterparty.accountId != null) {
-    return `/individuals/${counterparty.accountId}/counterparties/${counterparty.id}`;
+  } else if (counterparty.individualId != null) {
+    return `/individuals/${counterparty.individualId}/counterparties/${counterparty.id}`;
   } else {
     return null;
   }
