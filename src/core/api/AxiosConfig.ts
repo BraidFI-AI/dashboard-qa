d002c1bb98ce74fd198620e21aa9395e7d3b665c
Usama Hafeez <hafeezkardar000@gmail.com>
2025-12-17 18:00:17 +0500

    reverted test code

--- src/core/api/AxiosConfig.ts
+++ src/core/api/AxiosConfig.ts
@@ -1,5 +1,5 @@
 import { AxiosRequestConfig } from "axios";
-// import url from "../../url.json";
+import url from "../../url.json";
 
 // https://api.prod.braid.zone
 // https://api.dev.braid.zone
@@ -7,7 +7,7 @@ import { AxiosRequestConfig } from "axios";
 class AxiosConfig {
   static getConfig(): AxiosRequestConfig {
     const config: AxiosRequestConfig = {
-      baseURL: "https://api.development.braid.zone",
+      baseURL: url.url,
     };
 
     return config;
